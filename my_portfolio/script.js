7b8114bec670f721cca6ccde2874269f6d7fe131
onyilo benedict <153833656+OnyiloBenedictAmeh@users.noreply.github.com>
2023-12-19 13:01:20 +0100

    my my_portfolio

---
+++ my_portfolio/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new typed(".typing",{
+    strings:["","web Designer","Web Developer","Graphics Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
